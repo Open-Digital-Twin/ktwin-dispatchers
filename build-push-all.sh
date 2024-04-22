eab9aaca00da01e85e6b4cd44ea106ab1cf98523
agwermann <alexandrewermann@gmail.com>
2024-04-21 21:33:35 -0300

    chore: add build-push script

---
+++ build-push-all.sh
@@ -0,0 +1,7 @@
+# Build
+docker build -t ghcr.io/open-digital-twin/ktwin-mqtt-dispatcher:0.1 --build-arg SERVICE_NAME=mqtt-dispatcher .
+docker build -t ghcr.io/open-digital-twin/ktwin-cloud-event-dispatcher:0.1 --build-arg SERVICE_NAME=cloud-event-dispatcher .
+
+# # Push
+docker push ghcr.io/open-digital-twin/ktwin-mqtt-dispatcher:0.1
+docker push ghcr.io/open-digital-twin/ktwin-cloud-event-dispatcher:0.1
